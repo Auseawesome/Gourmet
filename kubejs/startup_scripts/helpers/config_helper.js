9d211785419f4efd8d47dc772f1a48d4cf885d13
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2025-06-30 11:30:45 +0100

    Added a pack config

---
+++ kubejs/startup_scripts/helpers/config_helper.js
@@ -0,0 +1,39 @@
+// priority: 1000
+
+global.configHelper = {}
+
+global.config = JsonIO.read("kubejs/config/pack.json")
+
+global.defaultConfig = {
+    "recipeLogging": false
+}
+
+global.configHelper.writeConfig = () => {
+    JsonIO.write("kubejs/config/pack.json", global.config)
+}
+
+global.configHelper.resetConfig = () => {
+    global.config = global.defaultConfig
+    global.configHelper.writeConfig()
+}
+
+global.configHelper.defaultValue = (key) => {
+    global.config[key] = global.defaultConfig[key]
+    global.configHelper.writeConfig()
+}
+
+global.configHelper.setValue = (key, value) => {
+    global.config[key] = value
+    global.configHelper.writeConfig()
+}
+
+global.configHelper.getValue = (key) => {
+    if (!Object.keys(global.config).includes(key)) {
+        global.configHelper.defaultValue(key)
+    }
+    return global.config[key]
+}
+
+if (global.config == null) {
+    global.configHelper.resetConfig()
+}
