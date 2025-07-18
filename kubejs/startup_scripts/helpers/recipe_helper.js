13ccc367779bb2cf9c73e095d58b7817ea3b5ced
Auseawesome <ajthomasbtb@outlook.com>
2025-07-18 22:03:23 +0100

    Improved recipe helper
    Added helpers for results, fluid ingredients and fluid results

--- kubejs/startup_scripts/helpers/recipe_helper.js
+++ kubejs/startup_scripts/helpers/recipe_helper.js
@@ -17,3 +17,44 @@ global.recipeHelper.itemOrTag = (id) => {
 global.recipeHelper.itemOrTagArray = (idArray) => {
     return idArray.map(global.recipeHelper.itemOrTag)
 }
+
+global.recipeHelper.result = (id) => {
+    return {
+        "id": id
+    }
+}
+
+global.recipeHelper.resultArray = (idArray) => {
+    return idArray.map(global.recipeHelper.result)
+}
+
+global.recipeHelper.fluidIngredient = (ingredient) => {
+    if (ingredient[0].startsWith("#")) {
+        return {
+            "type": "fluid_tag",
+            "amount": ingredient[1],
+            "fluid_tag": ingredient[0]
+        }
+    } else {
+        return {
+            "type": "fluid_stack",
+            "amount": ingredient[1],
+            "fluid": ingredient[0]
+        }
+    }
+}
+
+global.recipeHelper.fluidIngredientArray = (ingredientArray) => {
+    return ingredientArray.map(global.recipeHelper.fluidIngredient)
+}
+
+global.recipeHelper.fluidResult = (result) => {
+    return {
+        "amount": result[1],
+        "id": result[0]
+    }
+}
+
+global.recipeHelper.fluidResultArray = (resultArray) => {
+    return resultArray.map(global.recipeHelper.fluidResult)
+}
